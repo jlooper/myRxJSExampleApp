80293c4f06f80c068ceca25ad17705d57ba7f9a5
jlooper <jen.looper@gmail.com>
2017-01-10 21:37:13 -0500

    code cleanup

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,30 +1,26 @@
 import { Component } from "@angular/core";
 import { ActivityService } from "./app.service";
-import {Observable} from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
     selector: "my-app",
     templateUrl: "app.component.html"
 })
-export class AppComponent {   
+export class AppComponent {
 
     public activities$: Observable<any>;
     data: any;
     error: any;
 
-    constructor(private activityService: ActivityService) { 
+    constructor(private activityService: ActivityService) {
         this.activityService.getActivities().subscribe(
-        data => {
-            this.activities$ = data;
-        },
-        error => {
-            this.error = error;
-        });
+            data => {
+                this.activities$ = data;
+            },
+            error => {
+                this.error = error;
+            });
 
-        
     }
 
-    
-
-   
 }    
