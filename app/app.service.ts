1ee43d1d7e928f5f0f64a71ec82724d76c197a24
jlooper <jen.looper@gmail.com>
2017-01-10 21:31:09 -0500

    a bit more cleanup

--- app/app.service.ts
+++ app/app.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Inject, NgZone } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
