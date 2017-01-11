6e444945ef825b1aa790705ccf24f45896b4412b
jlooper <jen.looper@gmail.com>
2017-01-10 21:29:59 -0500

    code cleanup

--- app/app.service.ts
+++ app/app.service.ts
@@ -1,23 +1,17 @@
-import {Injectable, Inject, NgZone} from '@angular/core';
-import {Http} from '@angular/http';
-import {Observable} from 'rxjs/Observable';
-import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import { Injectable, Inject, NgZone } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/share';
 
 @Injectable()
 export class ActivityService {
 
-constructor (
-    private http: Http
-    ){}
+    constructor(
+        private http: Http
+    ) {}
 
-private handleError(){
-    console.log("error");
-}
- public getActivities(): Observable<any> {
-    return this.http.get('https://api.github.com/users/jlooper/events')
-                  .map(res=>res.json())
-                  //.catch(this.handleError);
- }
+    public getActivities(): Observable<any> {
+        return this.http.get('https://api.github.com/users/jlooper/events')
+            .map(res => res.json())
+    }
 }
